3db630cb37fac07637807d1a43883af52d066cec
paugier <pierre.augier@univ-grenoble-alpes.fr>
2026-05-11 23:15:11 +0200

    force_inline and bench

--- loop-fusion/bench_numpy.py
+++ loop-fusion/bench_numpy.py
@@ -6,9 +6,14 @@ size = 1000000
 
 arr = np.ones(size)
 
+print("add1:")
 t_start = time()
-
 for _ in range(100):
-    res = arr + arr + arr + arr
+    res = arr + arr
+print(f"{time() - t_start:.3f} s")
 
+print("add4:")
+t_start = time()
+for _ in range(100):
+    res = arr + arr + arr + arr + arr
 print(f"{time() - t_start:.3f} s")
